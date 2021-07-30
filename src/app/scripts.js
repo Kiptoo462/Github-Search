b8541c5d4c08e0b1901c8cb5fad5ca2bd282a391
Kiptoo Victor <victor.kiptoo@student.moringaschool.com>
2021-07-30 09:37:44 +0300

    Auto-generated commit [ci skip]

--- src/app/scripts.js
+++
@@ -1,9 +0,0 @@
-
-  $(window).scroll(function() {
-    if($(this).scrollTop() > 60)  /*height in pixels when the navbar becomes non opaque*/ 
-    {
-        $('.nav.opaque-navbar').addClass('.opaque');
-    } else {
-        $('.nav.opaque-navbar').removeClass('.opaque');
-    }
-});
--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: "https://api.github.com/",
-  accessToken:'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'
-};
